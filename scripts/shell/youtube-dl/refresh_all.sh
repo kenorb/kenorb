17a9b7403de7729863c08262e9456940b88ec150
Rafal <kenorb@gmail.com>
2015-02-26 18:03:35 +0000

    Few minor improvements.

--- scripts/shell/youtube-dl/refresh_all.sh
+++ scripts/shell/youtube-dl/refresh_all.sh
@@ -1,1 +1,1 @@
-find . -name refresh.sh -exec sh -c 'cd -P -- "$(dirname -- "{}")" && pwd -P && sh refresh.sh' \;
+find . -name .refresh.sh -exec sh -c 'cd -P -- "$(dirname -- "{}")" && pwd -P && sh .refresh.sh' \;
