9de45a0c52792fa0ddba6c53ecbd454bae34f11b
Rafal <kenorb@gmail.com>
2015-03-01 16:29:07 +0000

    Improving yql_query.sh with better code syntax.

--- scripts/shell/yql_query.sh
+++ scripts/shell/yql_query.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
-# Script to execute YQL query via Yahoo API.
+#!/bin/bash
+# Script to execute YQL query via Yahoo API from the command line.
 # Usage: ./yql_query.sh query (format) (curl-args)
 # Examples:
 #  ./yql_query.sh 'SELECT * FROM html WHERE url = "www.example.com"'
 #  echo 'SELECT div FROM html WHERE url = "www.example.com"' | ./yql_query.sh - xml -v
 
-[ "$1" != "-" ] && QUERY=$1 || QUERY=$(cat -)           # Read query from 1st argument or stdin (-).
-[ "$2" ] && { FORMAT="$2" && shift; }  || FORMAT="json" # Read format from argument (json/xml) or set 'json' as default.
-API_URL="http://query.yahooapis.com/v1/public/yql"
-shift
-curl -G --data-urlencode "q=$QUERY" "$API_URL?format=$FORMAT" $*
+[ "$1" != "-" ] && query="$1" || query=$(cat -)   # Read query from 1st argument or stdin (-).
+format=${2:-xml}                                  # Read format (default to xml).
+# test -f "$HOME/.yqlrc" && source <(grep = $HOME/.yqlrc)  # Load extra values from ~/.yqlrc if exist.
+url_api="http://query.yahooapis.com/v1/public/yql"
+curl -G --data-urlencode "q=$query" "$url_api?format=$format" ${@:3}
