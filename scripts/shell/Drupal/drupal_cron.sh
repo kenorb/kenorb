f94e45a9d2673421e95b6825c19e7700c0a28885
Rafal <kenorb@gmail.com>
2015-04-10 22:40:22 +0100

    Added HOST variable into drupal_cron.sh.

--- scripts/shell/Drupal/drupal_cron.sh
+++ scripts/shell/Drupal/drupal_cron.sh
@@ -4,7 +4,8 @@
 # - Suggested location: /etc/cron.hourly
 # - Override variables in /etc/default/drupal
 # - For Basic Authentication, use AUTH.
-CRONKEY="CHANGE_THIS" # Check: admin/config/system/cron
+HOST="http://example.com"
+CRON_KEY="CHANGE_THIS" # Check: admin/config/system/cron
 #AUTH="-u guest:guest"
 [ -e /etc/default/drupal ] && . /etc/default/drupal
-curl -s $AUTH "http://example.com/cron.php?cron_key=$CRONKEY"
+curl -s $AUTH "http://$HOST/cron.php?cron_key=$CRON_KEY"
