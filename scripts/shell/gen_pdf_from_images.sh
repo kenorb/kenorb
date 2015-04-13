41bbc1b0f7abaea2afde4e171c7a99cb8a792da2
Rafal <kenorb@gmail.com>
2015-04-13 20:30:58 +0100

    Improved gen_pdf_from_images.sh by changing the approach.

--- scripts/shell/gen_pdf_from_images.sh
+++ scripts/shell/gen_pdf_from_images.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Script scanning directories for consecutive PNG/JPEG files and generating PDF files.
-find . -type d -exec sh -c 'cd "{}"; ls *010.png 2> /dev/null && convert `ls *.{png,jpg}` -adjoin "`basename \"{}\"`.pdf" && ls *.pdf && echo rm -v *.png *.jpg ' \;
+#find . -type d -exec sh -c 'cd "{}"; ls *010*.png 2> /dev/null && convert `ls *.{png,jpg,gif}` -adjoin "`basename \"{}\"`.pdf" && ls *.pdf && echo rm -v *.png *.jpg ' \;
+find . \( -name \*010\*.jpg -o -name \*010\*.png \) -exec sh -c 'cd "`dirname "{}"`" && pwd && convert `ls *.{png,jpg,gif} 2> /dev/null` -adjoin "$(basename "`pwd`").pdf" && ls *.pdf && rm -v *.png *.jpg' \;
