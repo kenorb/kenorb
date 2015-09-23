b1a8332ef7a4ea21c0c019ad91a7804eb70e37dc
kenorb <kenorb@gmail.com>
2015-09-23 10:52:44 +0100

    Added shell script to pull all git repos at once.

--- scripts/shell/wget/.refresh.sh
+++ scripts/shell/wget/.refresh.sh
@@ -1,2 +1,2 @@
-alias wget-all='wget --user-agent=Mozilla -e robots=off --content-disposition --mirror --convert-links -E -K -N -r -nc -c'
+alias wget-all='wget --user-agent=Mozilla -e robots=off --content-disposition -m --convert-links -E -K -nc -c'
 wget-all http://www.zamrize.org/wrapp/ | tee .output.log
