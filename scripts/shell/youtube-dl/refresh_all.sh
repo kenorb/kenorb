2ff0135a8dbbd1e6ce582a38404f124180196c2c
kenorb <kenorb@gmail.com>
2015-10-25 22:39:15 +0000

    Simplyfing refresh_all.sh.

--- scripts/shell/youtube-dl/refresh_all.sh
+++ scripts/shell/youtube-dl/refresh_all.sh
@@ -1,1 +1,6 @@
-find . -name .refresh.sh -exec sh -c 'cd -P -- "$(dirname -- "{}")" && pwd -P && sh .refresh.sh' \;
+#!/bin/sh
+# Execute .refresh.sh in each directory recursively.
+find . -name .refresh.sh -execdir sh .refresh.sh ';'
+
+# Longer version:
+# find . -name .refresh.sh -exec sh -c 'cd -P -- "$(dirname -- "{}")" && pwd -P && sh .refresh.sh' \;
