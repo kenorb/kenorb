d656999e69688360c7b1686793827e8cf7a4bb42
Rafal <kenorb@users.noreply.github.com>
2016-11-01 22:05:29 +0000

    Adds default git editor

--- scripts/shell/git_pull_all.sh
+++ scripts/shell/git_pull_all.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Script to pull all git repositories to the recent version.
 ROOT=$(git rev-parse --show-toplevel 2> /dev/null)
+export GIT_EDITOR=true
 find "$ROOT" -name .git -type d -execdir git pull -v ';'
