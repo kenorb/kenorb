573f2210e7c325108041d8f347846755855b442b
kenorb <kenorb@users.noreply.github.com>
2018-04-20 11:56:01 +0100

    Adds --write-annotations

--- scripts/shell/youtube-dl/.refresh.sh
+++ scripts/shell/youtube-dl/.refresh.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-alias youtube-dl="youtube-dl -R5 -i --write-description  --all-subs --write-info-json --write-thumbnail -o '%(playlist)s/%(title)s-%(id)s.%(ext)s'"
+alias youtube-dl="youtube-dl -R5 -i --all-subs --write-thumbnail --write-description --write-info-json --write-annotations -o '%(playlist)s/%(title)s-%(id)s.%(ext)s'"
 youtube-dl http://www.youtube.com/user/Google | tee .output.log
